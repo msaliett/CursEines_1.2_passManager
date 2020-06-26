14fa7dc72912613bf5800b0fbdc5767e039f0ea1
Roser <msaliett@xtec.cat>
2020-06-26 16:06:08 +0200

    v1

--- PassManager/src/PassManager.java
+++ PassManager/src/PassManager.java
@@ -1,8 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+import cat.proven.Password;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+
 
 /**
  *
@@ -14,7 +16,28 @@ public class PassManager {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+
+         Password p = new Password();
+        boolean b;
+        
+        List<String> testData = new ArrayList<>();
+        
+        testData.add("");
+        testData.add("curt");
+        testData.add("curt1");
+        testData.add("passsensenum");
+        testData.add("1234567890");
+        testData.add("            ");
+        testData.add("1validpass");
+        
+        System.out.println("Test");
+        for (String word:testData){
+            System.out.println("Validate word " + word + " = " + p.validate(word));
+        }
+    
+        
+    
     }
     
 }
--- PassManager/src/cat/proven/Password.java
+++ PassManager/src/cat/proven/Password.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cat.proven;
 
 /**
@@ -10,5 +6,23 @@ package cat.proven;
  * @author roser
  */
 public class Password {
+
+    public Password() {
+    }
     
+    /**
+     * Validates according security requirements password strength
+     * @param toValidate
+     * @return true if passwords satisfy security requirements
+     */
+    public boolean validate(String toValidate){
+            boolean b = false;
+            if (toValidate.length()< 8) {
+                b= false;
+            } else {
+            b = (toValidate.chars().anyMatch(Character::isLetter)) && 
+                    (toValidate.chars().anyMatch(Character::isDigit));
+            }
+            return b;
+    }
 }
