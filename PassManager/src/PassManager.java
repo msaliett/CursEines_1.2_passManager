f28687193977bc56d5770a12f73d3d618f633b13
Roser <msaliett@xtec.cat>
2020-06-26 18:15:56 +0200

    V2

--- PassManager/src/PassManager.java
+++ PassManager/src/PassManager.java
@@ -23,13 +23,33 @@ public class PassManager {
         
         List<String> testData = new ArrayList<>();
         
+        //Test V1
+//        testData.add("");
+//        testData.add("curt");
+//        testData.add("curt1");
+//        testData.add("passsensenum");
+//        testData.add("1234567890");
+//        testData.add("            ");
+//        testData.add("1validpass");
+//        
+        //Test V2
         testData.add("");
         testData.add("curt");
         testData.add("curt1");
         testData.add("passsensenum");
         testData.add("1234567890");
         testData.add("            ");
-        testData.add("1validpass");
+        testData.add("*1novalidpass");
+        testData.add("1novalidpass&");
+        testData.add("lletra+simbols");
+        testData.add("1234567*1234567");
+        testData.add("Lletra+Simb0ls");
+        testData.add("*Lletra+Simb0ls");
+        testData.add("Lletra+Simb0ls!");
+        testData.add("*L1etra+Simb0ls");
+        testData.add("L1etra+Simb0ls!");
+        testData.add("L1etra+Simb0ls");
+        
         
         System.out.println("Test");
         for (String word:testData){
--- PassManager/src/cat/proven/Password.java
+++ PassManager/src/cat/proven/Password.java
@@ -1,6 +1,9 @@
 
 package cat.proven;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author roser
@@ -17,11 +20,36 @@ public class Password {
      */
     public boolean validate(String toValidate){
             boolean b = false;
-            if (toValidate.length()< 8) {
+            // Pattern for special symbols (not letter nor digit).
+            Pattern pat = Pattern.compile("[^a-z0-9]", Pattern.CASE_INSENSITIVE);
+            
+            if (toValidate.length()< 10) {
                 b= false;
             } else {
-            b = (toValidate.chars().anyMatch(Character::isLetter)) && 
-                    (toValidate.chars().anyMatch(Character::isDigit));
+                // match all vs special symbol
+                Matcher m = pat.matcher(toValidate);
+                // match first char vs special symbol
+                Matcher begin = pat.matcher(toValidate.substring(0, 1));
+                // match last char vs special symbol
+                Matcher end = pat.matcher(toValidate.substring(toValidate.length()-1,toValidate.length()));
+                b = 
+                     
+                    //Minimum 2 digits    
+                    (toValidate.chars().filter(Character::isDigit).count()>1) &&
+                    // Some character
+                    (toValidate.chars().anyMatch(Character::isLetter))        &&   
+                    // Some upper case    
+                    (toValidate.chars().anyMatch(Character::isUpperCase))     &&
+                    // Some lower case    
+                    (toValidate.chars().anyMatch(Character::isLowerCase))     && 
+                    // a special symbol
+                    m.find()                                                  &&  
+                    // Avoid special symbol at first character
+                    !begin.find()                                             &&
+                    // Avoid special symbol at last character
+                    !end.find();
+                
+           
             }
             return b;
     }
